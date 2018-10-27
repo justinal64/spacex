5db626d7efa30c2bb9b10e75861499fb62835ca4
justin leggett <justin@codingrookie.com>
2018-10-26 22:43:29 -0500

    click now changes the carousel src

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit, Input } from "@angular/core";
 export class CarouselComponent implements OnInit {
   @Input()
   Carousel: any[];
+  CarouselImgSrc: String =
+    "https://cdn.teslarati.com/wp-content/uploads/2017/09/SpaceX-Moon-Base-SpaceX.jpg";
   constructor() {}
 
   ngOnInit() {
@@ -19,18 +21,21 @@ export class CarouselComponent implements OnInit {
       },
       {
         src:
-          "https://www.trbimg.com/img-5b846dd6/turbine/os-spacex-telstar18-launch-20180827",
+          // tslint:disable-next-line:max-line-length
+          "https://img.purch.com/h/1000/aHR0cDovL3d3dy5zcGFjZS5jb20vaW1hZ2VzL2kvMDAwLzA3My85MTUvb3JpZ2luYWwvZmFsY29uLWhlYXZleS1zcGFjZXgtYnVpbGRpbmcuanBn",
         description: "First passanger on lunar bfr mission"
       },
       {
         src:
-          "https://www.trbimg.com/img-5b846dd6/turbine/os-spacex-telstar18-launch-20180827",
+          // tslint:disable-next-line:max-line-length
+          "https://mediadc.brightspotcdn.com/dims4/default/b1b254f/2147483647/strip/true/crop/1920x1080+0+0/resize/1920x1080!/quality/90/?url=https%3A%2F%2Fmediadc.brightspotcdn.com%2F74%2F73%2F4c2b67b3e5e6fdc65dff3dedebb6%2F74aa12b9888e1ff8c6c5feb7f3f51268.jpg",
         description: "Making life Multiplanetary"
       }
     ];
   }
 
-  public test() {
-    console.log("test worked!!!!");
+  public changeCarousel(src: String) {
+    console.log("src = ", src);
+    this.CarouselImgSrc = src;
   }
 }
