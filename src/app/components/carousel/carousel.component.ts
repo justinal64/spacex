251b1cbb661ef3c0e3b6c7c186f9d2ee5e940f65
justin leggett <justin@codingrookie.com>
2018-10-26 22:15:41 -0500

    added an array to iterate over and added click event

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-carousel',
-  templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  selector: "app-carousel",
+  templateUrl: "./carousel.component.html",
+  styleUrls: ["./carousel.component.css"]
 })
 export class CarouselComponent implements OnInit {
-
-  constructor() { }
+  @Input()
+  Carousel: any[];
+  constructor() {}
 
   ngOnInit() {
+    this.Carousel = [
+      {
+        src:
+          "https://www.trbimg.com/img-5b846dd6/turbine/os-spacex-telstar18-launch-20180827",
+        description: "Nasa Astronauts on crew dragon"
+      },
+      {
+        src:
+          "https://www.trbimg.com/img-5b846dd6/turbine/os-spacex-telstar18-launch-20180827",
+        description: "First passanger on lunar bfr mission"
+      },
+      {
+        src:
+          "https://www.trbimg.com/img-5b846dd6/turbine/os-spacex-telstar18-launch-20180827",
+        description: "Making life Multiplanetary"
+      }
+    ];
   }
 
+  public test() {
+    console.log("test worked!!!!");
+  }
 }
